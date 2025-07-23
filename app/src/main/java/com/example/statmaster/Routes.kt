563e3c7bcc38f575508f236e4558b656ccd7e482
gorgontt <166929134+gorgontt@users.noreply.github.com>
2025-07-23 20:20:09 +0300

    Revert "no work"
    This reverts commit e15758b7f3d2af64d53c41723274629473870f82.

--- app/src/main/java/com/example/statmaster/Routes.kt
+++ app/src/main/java/com/example/statmaster/Routes.kt
@@ -1,9 +1,6 @@
 package com.example.statmaster
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavType
@@ -14,7 +11,6 @@ import androidx.navigation.navArgument
 import com.example.statmaster.auth.MainClass
 import com.example.statmaster.terver.DocumentationLevelTerVer
 import com.example.statmaster.terver.LevelsTerVer
-import io.github.jan.supabase.gotrue.auth
 
 sealed class Routes(val route: String) {
     object MainClass : Routes("main_class")
@@ -30,14 +26,10 @@ fun Navigation() {
     val navController = rememberNavController()
     val context = LocalContext.current
     val authManager = remember { AuthManager(context) }
-    val isLoggedIn by remember {
-        mutableStateOf(authManager.supabase.auth.currentSessionOrNull() != null)
-    }
 
     NavHost(
         navController = navController,
-        ///startDestination = Routes.MainClass.route
-        startDestination = if (isLoggedIn) Routes.MainContent.route else Routes.MainClass.route
+        startDestination = Routes.MainClass.route
     ) {
         composable(Routes.MainClass.route) { MainClass(navController) }
         composable(Routes.MainContent.route) { MainContent(navController) }
@@ -48,17 +40,6 @@ fun Navigation() {
         ) { backStackEntry ->
             val levelId = backStackEntry.arguments?.getInt("levelId")
             DocumentationLevelTerVer(navController, levelId)
-        }
-    }
-
-    LaunchedEffect(Unit) {
-        authManager.supabase.auth.sessionStatus.collect { session ->
-            if (session != null && navController.currentDestination?.route != Routes.MainContent.route) {
-                navController.navigate(Routes.MainContent.route) {
-                    popUpTo(navController.graph.startDestinationId)
-                    launchSingleTop = true
-                }
-            }
         }
     }
 }
