a2e42f73ba521ae8f000a64eb8a602ce536048cd
gorgontt <166929134+gorgontt@users.noreply.github.com>
2026-07-27 23:43:01 +0300

    Revert "no work"
    This reverts commit 4cd89b05c30413e91d0286f1782cb916f9c94144.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,6 @@ dependencies {
     implementation(libs.androidx.navigation.compose.android)
     implementation(libs.play.services.drive)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.ui.text)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
